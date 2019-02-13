e31dedbb1a6a521fc8c91e074f3e85c401ddef7a
fucknamel <1047760450@qq.com>
2019-02-13 11:15:35 +0800

    get_category finished

--- src/main/java/com/cordyceps/controller/backend/CategoryManageController.java
+++ src/main/java/com/cordyceps/controller/backend/CategoryManageController.java
@@ -43,5 +43,18 @@ public class CategoryManageController {
         }
     }
 
-
+    @RequestMapping(value = "get_category.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse getChildrenCategory(HttpSession session,@RequestParam(value = "categoryId", defaultValue = "0") Integer categoryId){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user == null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登陆，请登录");
+        }
+        if(iUserService.checkAdminRole(user).isSuccess()){
+            //查询子节点的category信息，并且不递归，保持平级
+            return iCategoryService.getChildrenCategory(categoryId);
+        }else {
+            return ServerResponse.createByErrorMessage("无权限操作，需要管理员权限");
+        }
+    }
 }
